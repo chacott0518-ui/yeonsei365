57231227aad158119e3c3c554dd4214f9572783d
김세준 <chacott0518@gmail.com>
2026-04-06 13:07:43 +0900

    feat: 블로그 공통 OG 이미지 layout 추가

---
+++ app/blog/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  openGraph: {
+    images: [
+      {
+        url: 'https://www.yeonsei365.com/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: '연세365산부인과 임신중절수술 비용 안내',
+      },
+    ],
+  },
+}
+
+export default function BlogLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
+}
