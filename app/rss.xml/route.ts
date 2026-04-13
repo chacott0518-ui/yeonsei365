69ce9f2e2bad69d19872e539a9ebcbe700abd4ec
김세준 <chacott0518@gmail.com>
2026-04-14 02:35:21 +0900

    fix: RSS 캐시 원복

--- app/rss.xml/route.ts
+++ app/rss.xml/route.ts
@@ -66,7 +66,7 @@ ${rssItems}
   return new Response(rssFeed, {
     headers: {
       'Content-Type': 'application/xml; charset=utf-8',
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Cache-Control': 'public, max-age=3600, stale-while-revalidate=86400',
     },
   });
 }
